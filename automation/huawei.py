acf9d4f5529b4147d70644a37d0cf4d61fcbd09b
chenhanqing <2036244560@qq.com>
2019-12-20 17:32:56 +0800

    hh

--- automation/huawei.py
+++ automation/huawei.py
@@ -3,35 +3,38 @@ from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 driver=webdriver.Chrome('D:\chromedriver_win32\chromedriver.exe')
 driver.get('https://www.vmall.com/')
-menu=['智能手机','笔记本','平板','穿戴设备','智能家居','更多产品','软件应用','服务与支持']
-for one in menu:
-    eles = driver.find_elements_by_partial_link_text(f'{one}')
-    if eles == []:
-        print(f' "华为官网" 页面上无{one}')
-    else:
-        print(f' "华为官网" 页面上有{one}')
-ele = driver.find_element_by_id('zxnav_1')
+currnt_handle=driver.current_window_handle
+driver.find_element_by_link_text('华为官网').click()
+handle=driver.window_handles
+coMenu=['智能手机','笔记本','平板','穿戴设备','智能家居','更多产品','软件应用','服务与支持']
+vmallMenu=['平板电脑', '笔记本电脑', '笔记本配件']
+def menu_main(menu):
+    driver.switch_to.window(handle[1])
+    for one in menu:
+        eles = driver.find_elements_by_partial_link_text(f'{one}')
+        if eles == []:
+            print(f' "华为官网" 页面上无{one}')
+        else:
+            print(f' "华为官网" 页面上有{one}')
+def menu_vmall(menu):
+    driver.switch_to.window(handle[0])
+    ele = driver.find_element_by_id('zxnav_1')
+    # 鼠标移到悬停元素上
+    ActionChains(driver).move_to_element(ele).perform()
+    mes=driver.find_elements_by_xpath('// *[@id = "zxnav_1"] // *[@class="subcate-item"]/a/span')
+    eleTexts = [me.text for me in mes]
+    temp=0
+    for i in eleTexts:
+        for j in menu:
+            temp+=1
+            if i==j:
+                print(f'"笔记本&平板" 处上有{j}')
+                break
+            elif temp==len(menu):
+                print(f'"笔记本&平板" 处上无{j}')
+#driver.quit()
 
-# 鼠标移到悬停元素上
-ActionChains(driver).move_to_element(ele).perform()
-mes=driver.find_elements_by_xpath('// *[@id = "zxnav_1"] // *[@class="subcate-item"]/a/span')
-eleTexts = [me.text for me in mes]
-list=['平板电脑', '笔记本电脑', '笔记本配件']
-temp=0
-for i in eleTexts:
-    for j in list:
-        temp+=1
-        if i==j:
-            print(f'"笔记本&平板" 处上有{j}')
-            break
-        elif temp==len(list):
-            print(f'"笔记本&平板" 处上无{j}')
-
-
-    # if me == []:
-    #     print(f'"笔记本&平板" 处上无{li}')
-    # else:
-    #     print(f'"笔记本&平板" 处上有{li}')
-driver.quit()
-
-
+if __name__ == '__main__':
+    menu_main(coMenu)
+    menu_vmall(vmallMenu)
+    driver.quit()
--- automation/uploadfile.py
+++ automation/uploadfile.py
@@ -1,14 +1,24 @@
 from selenium import webdriver
 import win32com.client
-import win32api
+import win32com
+import win32gui
 import time
 driver=webdriver.Chrome('D:\chromedriver_win32\chromedriver.exe')
 driver.get('https://tinypng.com/ ')
 driver.find_element_by_class_name('icon').click()
 time.sleep(5)
+dialog = win32gui.FindWindow('#32770', '文件上传')  # 对话框
+ComboBoxEx32 = win32gui.FindWindowEx(dialog, 0, 'ComboBoxEx32', None)
+ComboBox = win32gui.FindWindowEx(ComboBoxEx32, 0, 'ComboBox', None)
+Edit = win32gui.FindWindowEx(ComboBox, 0, 'Edit', None)  # 上面三句依次寻找对象，直到找到输入框Edit对象的句柄
+button = win32gui.FindWindowEx(dialog, 0, 'Button', None)  # 确定按钮Button
 
-shell=win32com.client.Dispatch("WScript.Shell")
-shell.Sendkeys(r'C:\Users\Administrator\Desktop\157673009.jpg'+'\n')
-print('############3')
+win32gui.SendMessage(Edit, win32com.WM_SETTEXT, None, r'C:\Users\Administrator\Desktop\157673009.jpg')  # 往输入框输入绝对地址
+win32gui.SendMessage(dialog, win32com.WM_COMMAND, 1, button)  # 按button
+driver.quit()
+# shell=win32com.client.Dispatch("WScript.Shell")
+# shell.Sendkeys(r'C:\Users\Administrator\Desktop\157673009.jpg'+'\r')
+# win32api.keybd_event(13,0,0,0)
+# print('############3')
 #driver.find_element_by_class_name('progress success')
 # driver.quit()
