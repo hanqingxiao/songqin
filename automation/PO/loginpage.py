a9156c953e9e0db55f2bccd2d2826fe424f52956
chenhanqing <2036244560@qq.com>
2019-12-25 20:15:03 +0800

    c

--- automation/PO/loginpage.py
+++ automation/PO/loginpage.py
@@ -3,24 +3,25 @@ import time
 class LoginPage:
 
     def login(self,username,password):
-        driver = webdriver.Chrome('D:\chromedriver_win32\chromedriver.exe')
+        driver = webdriver.Chrome()
         driver.implicitly_wait(10)
         driver.get('http://localhost/mgr/login/login.html')
         driver.find_element_by_id('username').send_keys(username)
         driver.find_element_by_id('password').send_keys(password)
         driver.find_element_by_css_selector('[class="btn btn-success"]').click()
         #driver.quit()
+        #返回下一个流程页面
 
 class CoursePage():
     def __init__(self):
         #初始化方法里面打开浏览器，访问网页
-        self.driver=webdriver.Chrome('D:\chromedriver_win32\chromedriver.exe')
+        self.driver=webdriver.Chrome()
+        self.driver.implicitly_wait(10)
         self.driver.get('http://localhost/mgr/ps/mgr/index.html#/')
 
     def add_Course(self,name,desc,idx):
-        time.sleep(3)
+        self.driver.implicitly_wait(3)
         self.driver.find_element_by_css_selector('[class="btn btn-blue btn-outlined btn-md"]').click()
-        time.sleep(1)
         self.driver.find_element_by_css_selector('[class="form-control ng-pristine'
                                                  ' ng-untouched ng-valid"]').send_keys(name)
         self.driver.find_element_by_css_selector('[class="form-control ng-pristine'
@@ -29,6 +30,7 @@ class CoursePage():
                                                  ' ng-untouched ng-valid"]').send_keys(idx)
         self.driver.find_element_by_css_selector('[class="btn btn-green btn-outlined'
                                                  ' btn-xs"]').click()
+        self.driver.implicitly_wait(10)
     def dele_Course(self):
         self.driver.implicitly_wait(1)
         while 1:
